872e701ad2fddb08af927ade0abe6c6fb8267528
MikeXXd <122735478+MikeXXd@users.noreply.github.com>
2023-10-06 22:47:02 +0200

    styling tuning

--- Enjoy-The-Photo2/src/App.tsx
+++ Enjoy-The-Photo2/src/App.tsx
@@ -1,35 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid-container-home">
+      <div className="header-and-nav-bar">
+        <span className="pre-header hide-on-small-device">
+          Breath in the depth of colors and geometry, jump in and enjoooooy the{" "}
+          <a href="#">Story</a>
+        </span>
+        <header className="header">
+          <h1>Enjoy the Photos2</h1>
+        </header>
+        <nav className="nav-bar">
+          <ul>
+            <li>
+              <a href="#">Galery</a>
+            </li>
+            <li>
+              <a href="#"><em>u</em>Story</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Setting</a>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <section className="search-bar sticky">
+        <button className="btn">previous</button>
+        <input type="text" />
+        <button className="btn ">Search</button>
+        <button className="btn ">next</button>
+      </section>
+      <main className="grid-container-masonri">
+        <div className="img-container tall">
+          <img className="img " src="https://source.unsplash.com/cssvEZacHvQ" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/oR0uERTVyD0" />
+        </div>
+        <div className="img-container wide">
+          <img className="img " src="https://source.unsplash.com/01_igFr7hd4" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/O-8Fmpx7HqQ" />
+        </div>
+        <div className="img-container wide">
+          <img className="img " src="https://source.unsplash.com/cfQEO_1S0Rs" />
+        </div>
+        <div className="img-container tall">
+          <img className="img " src="https://source.unsplash.com/FpATXvTxEI4" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/uf4oyaimWwg" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/EwKXn5CapA4" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/rTZW4f02zY8" />
+        </div>
+        <div className="img-container wide tall">
+          <img className="img " src="https://source.unsplash.com/vltMzn0jqsA" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/aQOe0Ri267U" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/rMmibFe4czY" />
+        </div>
+        <div className="img-container tall">
+          <img className="img " src="https://source.unsplash.com/-Bq3TeSBRdE" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/IicyiaPYGGI" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/jR4Zf-riEjI" />
+        </div>
+        <div className="img-container wide tall ">
+          <img className="img " src="https://source.unsplash.com/IJ25m7fXqtk" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/eOpewngf68w" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/h54G2wyDadc" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/_aVKjJhFdn0" />
+        </div>
+        <div className="img-container">
+          <img className="img" src="https://source.unsplash.com/uaJLLOhY2yU" />
+        </div>
+      </main>
+      <footer className="footer">
+        <span>
+          Created by <a href="#">MikeXd</a> 2023
+        </span>
+      </footer>
+    </div>
+  );
+};
 
-export default App
+export default App;

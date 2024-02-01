ccf34c254c8344179913a06709d53d93ae817718
MikeXXd <122735478+MikeXXd@users.noreply.github.com>
2024-02-01 15:15:17 +0100

    ActiveText feature implemented

--- Enjoy-The-Photo2/src/components/ActiveText.tsx
+++ Enjoy-The-Photo2/src/components/ActiveText.tsx
@@ -1,32 +1,65 @@
+import { useNavigate } from "react-router-dom";
 import usePhotos from "../context/usePhotos";
+import { cc } from "../utils/cc";
+import { useEffect, useState } from "react";
 
 interface Props {
   children: string;
 }
 
 const ActiveText = ({ children }: Props) => {
-  const {setNewQuery } = usePhotos();
+    
   const words = children.split(" ");
-
-  const handleClick = (word: string) => {
-    setNewQuery(word);
-  };
 
   if (!words.length) return null;
 
   return (
     <>
       {words.map((word, index) => (
-        <span
-          key={index}
-          onClick={() => handleClick(word)}
-          className="active-text"
-        >
-          {word}{" "}
-        </span>
+        <ActiveWord key={index} word={word} />
       ))}{" "}
     </>
   );
 };
 
 export default ActiveText;
+
+
+
+
+
+interface ActiveWordProps {
+    word: string
+}
+
+const ActiveWord = ({word}: ActiveWordProps) => {
+    const navigate = useNavigate()
+    const [isGlittering, setIsGlittering] = useState(false)
+    const {setNewQuery } = usePhotos();
+
+    useEffect(() => {
+
+        const interval = setTimeout(() => {
+            setIsGlittering(true);
+        }, Math.floor(Math.random() * 9000) + 1000);
+
+        setIsGlittering(false);
+
+        return () => clearTimeout(interval);
+    }, []);
+
+    const handleClick = (word: string) => {
+        setNewQuery(word);
+        navigate("/");
+      };
+
+  return (
+    <span
+          onClick={() => handleClick(word)}
+          className={cc("active-text", isGlittering && "glittering")}
+        >
+          {word}{" "}
+        </span>
+  )
+}
+
